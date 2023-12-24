d45193e176d6b03155c39d941be8ad8f34720534
MattAku_ <153075689+MattAkou@users.noreply.github.com>
2023-12-24 20:30:52 +0100

    Update Accelero1.ino
    Now into a fonction called "checkIntegrity"
    Return true if a movement is detected (have to be in a loop with a delay of 250 to 500ms per check to be accurate enough).

--- Accelero1/Accelero1.ino
+++ Accelero1/Accelero1.ino
@@ -1,81 +1,65 @@
 #include <Arduino_LSM6DS3.h>
 
-//Paramètre qui setup des valeurs par défaut pour:
-int degreesX = 0;
-int degreesY = 0;
-int degreesXplus = 0;
-int degreesYplus =0;
 
-//Le SETUP "récupère" les données du capteur LSM6DS3
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+//La fonction checkIntegrity vérifie que la carte n'a pas été bougé lors de la capture d'images
+//On renvoit true si le mouvement est perçu, sinon false.
+bool checkIntegrity () {
 
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
-    while (1);
-  }
-  Serial.print("Accelerometer sample rate = ");
-  Serial.print(IMU.accelerationSampleRate());
-  Serial.println(" Hz");
-}
+  //Paramètre des valeurs par défaut:
+  int degreesX = 0;
+  int degreesY = 0;
+  int degreesXplus = 0;
+  int degreesYplus =0;
 
-//Interprétation toutes les 0.5s de l'inclinaison de la carte
-void loop() {
   float x, y, z;
 
+  //On auvre la lecture des données de l'accéléromètres
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x, y, z);
   }
 
-  //Conversion en degré
+  //Conversion en degré (°)
   x = 100*x;
   degreesX = x;
   y = 100*y;
   degreesY = y;
 
-  /**
-  Serial.print(degreesX);
-  Serial.print(" - ");
-  Serial.print(degreesXplus);
-  **/
+  // Ici on cherche à vérifié que entre 2 positions précédentes, 
+  // il n'y a pas une distance supérieur à 5° sur les axes X et Y
 
-
-  //Axe X :
-  Serial.println("\n");
-  Serial.print("\nAngle X = ");
-  Serial.print(degreesX);
-  Serial.print(" °");
-
-  //Compare l'inclinaison du capteur sur l'axe avec l'ancienne.
-  //Envoi un message dès qu'il y a un soucis
-  //Remet les paramètre à l'état d'origine pour éviter le spam
-  if (degreesXplus == 0) {
+  //On place la valeur de la première donnée capturé (degreesX ou degreesY)
+  //dans degreesXplus ou degreesYplus si elles valent 0
+  if ((degreesXplus == 0) || (degreesYplus == 0)) {
     degreesXplus = degreesX;
-  }
-  else if (abs(degreesXplus - degreesX) >= 5) {
-    Serial.print("\nIntégrité compromise ! sur X");
-    degreesXplus = 0;
-  }
-
-
-  //Axe Y :
-  Serial.print("\nAngle Y = ");
-  Serial.print(degreesY);
-  Serial.print(" °");
-
-
-  //Compare l'inclinaison du capteur sur l'axe avec l'ancienne.
-  //Envoi un message dès qu'il y a un soucis
-  //Remet les paramètre à l'état d'origine pour éviter le spam
-  if (degreesYplus == 0) {
     degreesYplus = degreesY;
   }
-  if (abs(degreesYplus - degreesY) >= 5) {
-    Serial.print("\nIntégrité compromise ! sur Y");
+
+  //On vérifit que la différence absolu entre la première mesure d'angle et celle juste après ne dépasse
+  //pas 5°. Cette valeur a été déterminé expérimentalement pour garantir un véritable mouvement volontaire.
+  //On remet alors les valeurs degreesXplus et degreesYplus à 0 pour éviter l'envoi répété de notification 
+  //si la carte reste en position "dégradé".
+  if ((abs(degreesXplus - degreesX) >= 5) || (abs(degreesYplus - degreesY) >= 5)) {
+    degreesXplus = 0;
     degreesYplus = 0;
+    return true;
   }
+  return false;
+}
 
-delay(500);
 
+//Structure d'utilisation de la fonction précédente
+void setup() {
+  // Serial.begin(9600);
+  // while (!Serial);
+
+  // if (!IMU.begin()) {
+  //   Serial.println("Failed to initialize IMU!");
+  //   while (1);
+  // }
+}
+
+//Interprétation toutes les 0.5s de l'inclinaison de la carte
+void loop() {
+  Serial.print(checkIntegrity());
+  delay(500);
 }
